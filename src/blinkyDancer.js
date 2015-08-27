a4bb04208cc6165f32e1f3981040de2643e3c392
taylorleh <taylorlehman111@gmail.com>
2015-08-26 17:35:06 -0700

    Added slidingdancer and modified blinkydancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,17 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+function BlinkyDancer(top, left, timeBetweenSteps){
+  this._oldstep = Dancer.prototype.step;
+  Dancer.apply(this, arguments);
+}
 
-  var oldStep = blinkyDancer.step;
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  blinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+BlinkyDancer.prototype.step = function() {
+  // We're supposed to call old step
+  this._oldstep();
 
-  return blinkyDancer;
+  // And then call blindancer.node.toggle
+  this.$node.toggle();
 };
+
--- src/init.js
+++ src/init.js
@@ -19,10 +19,9 @@ $(document).ready(function(){
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
